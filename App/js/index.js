c4c9fcf41fb7956d48c4343e49482dde8c250652
renbc <48265852+renbc@users.noreply.github.com>
2019-08-27 14:52:27 +0800

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -1,22 +1,88 @@
 xui.Class('App', 'xui.Module',{
     Instance:{
         iniComponents : function(){
-            // [[Code created by CrossUI RAD Tools
+            // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
-            append((new xui.UI.Layout())
-            .setHost(host,"ctl_layout5")
-            .setItems([{"id":"before", "pos":"before", "size":100, "min":50, "max":400, "locked":false, "folded":false, "hidden":false, "cmd":false}, {"id":"main", "min":10, "size":450}, {"id":"after", "pos":"after", "size":50, "min":20, "max":400, "locked":false, "folded":false, "hidden":false, "cmd":false}])
+            
+            append(
+                xui.create("xui.UI.Layout")
+                .setHost(host,"ctl_layout5")
+                .setItems([
+                    {
+                        "id":"before",
+                        "pos":"before",
+                        "size":100,
+                        "min":50,
+                        "max":400,
+                        "locked":false,
+                        "folded":false,
+                        "hidden":false,
+                        "cmd":false
+                    },
+                    {
+                        "id":"main",
+                        "min":10,
+                        "size":450
+                    },
+                    {
+                        "id":"after",
+                        "pos":"after",
+                        "size":50,
+                        "min":20,
+                        "max":400,
+                        "locked":false,
+                        "folded":false,
+                        "hidden":false,
+                        "cmd":false
+                    }
+                ])
             );
-
-            host.ctl_layout5.append((new xui.UI.Layout())
-            .setHost(host,"ctl_layout13")
-            .setItems([{"id":"before", "pos":"before", "size":150, "min":10, "locked":false, "folded":false, "hidden":false, "cmd":true}, {"id":"main", "min":10, "size":530}, {"id":"after", "pos":"after", "size":120, "min":10, "locked":false, "folded":false, "hidden":false, "cmd":false}])
-            .setType("horizontal")
-            , "main");
-
+            
+            host.ctl_layout5.append(
+                xui.create("xui.UI.Layout")
+                .setHost(host,"ctl_layout13")
+                .setItems([
+                    {
+                        "id":"before",
+                        "pos":"before",
+                        "size":150,
+                        "min":10,
+                        "locked":false,
+                        "folded":false,
+                        "hidden":false,
+                        "cmd":true
+                    },
+                    {
+                        "id":"main",
+                        "min":10,
+                        "size":530
+                    },
+                    {
+                        "id":"after",
+                        "pos":"after",
+                        "size":120,
+                        "min":10,
+                        "locked":false,
+                        "folded":false,
+                        "hidden":false,
+                        "cmd":false
+                    }
+                ])
+                .setType("horizontal"),
+                "main"
+            );
+            
+            host.ctl_layout13.append(
+                xui.create("xui.UI.Image")
+                .setHost(host,"xui_ui_image5")
+                .setLeft("22.5em")
+                .setTop("7.5em")
+                .setSrc("https://www.crossui.com/RAD/img/svg_demo.svg"),
+                "main"
+            );
+            
             return children;
-            // ]]Code created by CrossUI RAD Tools
+            // ]]Code created by CrossUI RAD Studio
         }
     }
 });
